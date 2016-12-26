a88cc9e5f4d3cb9f453963d4e486ff1ed606edcd
BugBuster99 <arvindj19962014@gmail.com>
2016-12-26 20:50:37 +0530

    Update Digit.c

--- Digit.c
+++ Digit.c
@@ -5,13 +5,13 @@ void main()
   printf("Enter the number");
   c=0;
   sum=0;
-  Scanf("%d",&n);
+  scanf("%d",&n);
   while(n>0)
   {
     sum=sum+(n%10);
     c++;
     n=n/10;
   }
-  printf("The sum of digits is: ",sum;);
-  printf("\nThe number of digits is: ",c);
+  printf("The sum of digits is: %d\n",sum);
+  printf("The number of digits is: %d",c);
 }
